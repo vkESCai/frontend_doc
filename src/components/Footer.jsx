be8b0d6c61f531db61f271ff68ecf8c698dea312
Vikash Kumar <vikashkumar@Vikashs-MacBook-Air.local>
2026-03-03 21:53:11 +0530

    adding frontend on git

---
+++ src/components/Footer.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='md:mx-10'>
+      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10  mt-40 text-sm'>
+
+        <div>
+          <img className='mb-5 w-40' src={assets.careConnect} alt="" />
+          <p className='w-full md:w-2/3 text-gray-600 leading-6'>Care Connect is built with one goal — to bring care closer to you. We connect patients with verified doctors, streamline appointments, and ensure a smooth healthcare experience built on trust, technology, and compassion.</p>
+        </div>
+
+        <div>
+          <p className='text-xl font-medium mb-5'>COMPANY</p>
+          <ul className='flex flex-col gap-2 text-gray-600'>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+
+        <div>
+          <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+          <ul className='flex flex-col gap-2 text-gray-600'>
+            <li>995552394822</li>
+            <li>vikash_dev@gmail.com</li>
+          </ul>
+        </div>
+
+      </div>
+
+      <div>
+        <hr />
+        <p className='py-5 text-sm text-center'>Copyright 2026 @ Care Connect.com - All Right Reserved.</p>
+      </div>
+
+    </div>
+  )
+}
+
+export default Footer
